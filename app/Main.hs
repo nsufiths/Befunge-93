2db09ae7f3b35f8c8282824b60ca985f1ee2708b
Michael <71266782+SiegKlas@users.noreply.github.com>
2021-04-03 17:41:59 +0700

    deleted additional brackets, improved input list

--- app/Main.hs
+++ app/Main.hs
@@ -4,12 +4,12 @@ import Lib
 import System.IO
 
 main = do
-    putStrLn "Type the name of the file:"
+    putStrLn "Type the name of the input file:"
     whatToOpen <- getLine
     handle <- openFile (whatToOpen) ReadMode
     contents <- hGetContents handle
     let linesOfFiles = lines contents
-    let newLinesOfFiles = (map (++ (replicate 80 ' ')) linesOfFiles) ++ (replicate (25 - length linesOfFiles) (replicate 80 ' '))
+    let newLinesOfFiles = map (\x -> x ++ replicate (80 - length x) ' ') linesOfFiles ++ replicate (25 - length linesOfFiles) (replicate 80 ' ')
     let myPtr = ProgramPtr { _code = newLinesOfFiles, _myStack = [], _direction = (0, 1), _location = (0, 0), _stringMode = False, _currentSymb = ' ', _rnd = 0, _board = [], _output = [] }
     mainCycle myPtr
     putStr "Finished!"
--- src/Lib.hs
+++ src/Lib.hs
@@ -38,15 +38,15 @@ mainCycle :: ProgramPtr -> IO ()
 mainCycle prog = do 
     myRandomNumber <- randomRIO (1, 4) :: IO Int
     let progAfterRnd = set rnd myRandomNumber $ saveChar prog
-    if (view currentSymb progAfterRnd) == '@' && (view stringMode progAfterRnd) == False
+    if view currentSymb progAfterRnd == '@' && view stringMode progAfterRnd == False
         then do outh <- openFile "output.txt" WriteMode
                 hPutStrLn outh (view output progAfterRnd)
                 hClose outh
                 return ()
         else do 
-                if elem (view currentSymb progAfterRnd) ("&~")
+                if elem (view currentSymb progAfterRnd) "&~"
                     then do 
-                            putStrLn (view output progAfterRnd)
+                            putStrLn $ view output progAfterRnd
                             cl <- getLine
                             let progAfterInputFromKeyboard = set board cl progAfterRnd
                             let progStatAfterStep = makeStep $ makeInstr (view currentSymb progAfterInputFromKeyboard) progAfterInputFromKeyboard
@@ -61,18 +61,18 @@ myTail list = tail list
 
 popFromStack :: ProgramPtr -> Int
 popFromStack prog
-    | (view myStack prog) == [] = 0
+    | view myStack prog == [] = 0
     | otherwise = head $ view myStack prog
 
 subPopFromStack :: ProgramPtr -> Int
 subPopFromStack prog
-    | (length (view myStack prog)) <= 1 = 0
-    | otherwise = (view myStack prog) !! 1
+    | length (view myStack prog) <= 1 = 0
+    | otherwise = view myStack prog !! 1
 
 subSubPopFromStack :: ProgramPtr -> Int
 subSubPopFromStack prog
-    | (length (view myStack prog)) <= 2 = 0
-    | otherwise = (view myStack prog) !! 2
+    | length (view myStack prog) <= 2 = 0
+    | otherwise = view myStack prog !! 2
 
 makeStep :: ProgramPtr -> ProgramPtr
 makeStep prog = set location newLocation prog
@@ -81,23 +81,23 @@ makeStep prog = set location newLocation prog
                           (dirY, dirX) = view direction prog
 
 saveChar :: ProgramPtr -> ProgramPtr
-saveChar prog = set currentSymb ((view code prog) !! posY !! posX) prog
+saveChar prog = set currentSymb (view code prog !! posY !! posX) prog
                     where (posY, posX) = view location prog
 
 makeInstr :: Char -> ProgramPtr -> ProgramPtr
 makeInstr ch prog
-    | (view stringMode prog) == True && ch /= '"' = set myStack newStack prog
+    | view stringMode prog == True && ch /= '"' = set myStack newStack prog
         where newStack = (ord ch) : (view myStack prog)
 makeInstr '^' prog = set direction up prog
 makeInstr 'v' prog = set direction down prog
 makeInstr '<' prog = set direction left prog
 makeInstr '>' prog = set direction right prog
 makeInstr '_' prog = 
-    if (popFromStack prog == 0) || ((view myStack prog) == [])
+    if popFromStack prog == 0 || view myStack prog == []
         then set myStack (myTail (view myStack prog)) . set direction right $ prog
         else set myStack (myTail (view myStack prog)) . set direction left $ prog
 makeInstr '|' prog = 
-    if (popFromStack prog == 0) || ((view myStack prog) == [])
+    if popFromStack prog == 0 || view myStack prog == []
         then set myStack (myTail (view myStack prog)) . set direction down $ prog
         else set myStack (myTail (view myStack prog)) . set direction up $ prog
 makeInstr '#' prog = makeStep prog
@@ -117,26 +117,26 @@ makeInstr 'g' prog = set myStack newStack prog
     where newStack = newPop : (drop 2 (view myStack prog))
           posY = subPopFromStack prog
           posX = popFromStack prog
-          newPop = ord ((view code prog) !! posY !! posX)
+          newPop = ord (view code prog !! posY !! posX)
 makeInstr '!' prog = set myStack newStack prog
     where newStack
             | popFromStack prog == 0 = 1 : (myTail (view myStack prog))
             | otherwise = 0 : (myTail (view myStack prog))
 makeInstr '`' prog = set myStack newStack prog
     where newStack
-            | (subPopFromStack prog) > (popFromStack prog) = 1 : (drop 2 (view myStack prog))
+            | subPopFromStack prog > popFromStack prog = 1 : (drop 2 (view myStack prog))
             | otherwise = 0 : (drop 2 (view myStack prog))
 makeInstr '.' prog = set myStack newStack . set output newOutput $ prog
-    where newOutput = (view output prog) ++ (show (popFromStack prog))
+    where newOutput = view output prog ++ show (popFromStack prog)
           newStack = myTail $ view myStack prog
 makeInstr ',' prog = set myStack newStack . set output newOutput $ prog
-    where newOutput = (view output prog) ++ [chr (popFromStack prog)]
+    where newOutput = view output prog ++ [chr (popFromStack prog)]
           newStack = myTail $ view myStack prog
 makeInstr '?' prog = set direction randDirection prog
     where randDirection
-            | (view rnd prog) == 1 = (-1,0)
-            | (view rnd prog) == 2 = (1,0)
-            | (view rnd prog) == 3 = (0,1)
+            | view rnd prog == 1 = (-1,0)
+            | view rnd prog == 2 = (1,0)
+            | view rnd prog == 3 = (0,1)
             | otherwise = (0,-1)
 makeInstr '&' prog = set myStack newStack prog
     where newStack = (read (view board prog) :: Int) : (view myStack prog)
@@ -146,10 +146,10 @@ makeInstr '"' prog
     | view stringMode prog == False = set stringMode True prog
     | otherwise = set stringMode False prog
 makeInstr ch prog
-    | elem ch ("0123456789") = set myStack newStack prog
+    | elem ch "0123456789" = set myStack newStack prog
         where newStack = (digitToInt ch) : (view myStack prog)
 makeInstr ch prog
-    | elem ch ("+-*/%") = set myStack newStack prog
+    | elem ch "+-*/%" = set myStack newStack prog
         where newStack = (operation ch subPop pop) : (drop 2 (view myStack prog))
               subPop = subPopFromStack prog
               pop = popFromStack prog
